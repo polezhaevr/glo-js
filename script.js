b0d2245269adae5f6e8f6866bc87cf971f1863d7
Roman <roman@Romans-MacBook-Pro.local>
2025-04-24 22:57:01 +0500

    feat: added sense for variable and added simply operation with variable

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive
+let title = "Проект по расчету стоимости работы",
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 10000,
+    rollback = 10,
+    fullPrice = 100000,
+    adaptive = true;
 
+console.log(" Тип данных переменной 'title':", typeof title, '\n',
+    "Тип данных переменной 'fullPrice':", typeof fullPrice, '\n',
+    "Тип данных переменной 'adaptive':", typeof adaptive);
 
-alert('Hello world');
-console.log('Hello world');
+console.log("Стоимость верстки экранов", screenPrice, "рублей");
+console.log("Стоимость разработки сайта", fullPrice, "рублей");
+console.log(screens.toLocaleLowerCase().split(' '));
+
+console.log("Процент отката посреднику за работу:", fullPrice * (rollback / 100))
